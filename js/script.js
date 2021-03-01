8277a92c680cf2a248daa439164f3e9da9b67951
Alexander Tyurin <alexturin@gmail.com>
2021-03-01 22:45:01 +0700

    DOM elements

--- js/script.js
+++ js/script.js
@@ -61,75 +61,126 @@ P.S. Функции вызывать не обязательно*/
 при помощи метода forEach вывести в консоль сообщения в таком виде:
 "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-"use strict";
+// "use strict";
 
-alert( +"Infinity" ); 
-const personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+// alert( +"Infinity" ); 
+// const personalMovieDB = {
+//   count: 0,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
 
-	start: function() {
-		let num;;
-		do{
-			num = parseInt(prompt("Сколько фильмоы вы уже посмотрели?", ""));
-		}
-		while (num === "" || num === null || isNaN(num));
-		this.count = num;
-	},
+// 	start: function() {
+// 		let num;
+// 		do{
+// 			num = parseInt(prompt("Сколько фильмоы вы уже посмотрели?", ""));
+// 		}
+// 		while (num === "" || num === null || isNaN(num));
+// 		this.count = num;
+// 	},
 
-  setMyFilms: function () {
-    for (let index = 0; index < 2; index++) {
-      let lastMovie;
-      let appraisal;
-      do {
-        lastMovie = prompt("Один из последних просмотенных фильмов?", "");
-      } while (lastMovie === null || lastMovie === "" || lastMovie.lenght > 50);
-      do {
-        appraisal = prompt("На сколько его оцените?", "");
-      } while (appraisal === null || appraisal === "" || appraisal.length > 50);
-      this.movies[lastMovie] = appraisal;
-    }
-  },
-  personalLevel: function () {
-    if (this.count < 10) {
-      alert("Просмотрено довольно мало фильмов");
-    } else if (this.count >= 10 && this.count <= 30) {
-      alert("Вы классический зритель");
-    } else if (this.count > 30) {
-      alert("Вы киноман");
-    } else {
-      alert("Произошла ошибка");
-    }
-  },
-  showMyDB: function () {
-    if (!this.privat) {
-      console.log(this);
-    }
-  },
+//   setMyFilms: function () {
+//     for (let index = 0; index < 2; index++) {
+//       let lastMovie;
+//       let appraisal;
+//       do {
+//         lastMovie = prompt("Один из последних просмотенных фильмов?", "");
+//       } while (lastMovie === null || lastMovie === "" || lastMovie.lenght > 50);
+//       do {
+//         appraisal = prompt("На сколько его оцените?", "");
+//       } while (appraisal === null || appraisal === "" || appraisal.length > 50);
+//       this.movies[lastMovie] = appraisal;
+//     }
+//   },
+//   personalLevel: function () {
+//     if (this.count < 10) {
+//       alert("Просмотрено довольно мало фильмов");
+//     } else if (this.count >= 10 && this.count <= 30) {
+//       alert("Вы классический зритель");
+//     } else if (this.count > 30) {
+//       alert("Вы киноман");
+//     } else {
+//       alert("Произошла ошибка");
+//     }
+//   },
+//   showMyDB: function () {
+//     if (!this.privat) {
+//       console.log(this);
+//     }
+//   },
 
-  writeYourGenres: function () {
-    let genre;
+//   writeYourGenres: function () {
+//     let genre;
 
-    for (let i = 1; i <= 3; i++) {
-      do genre = prompt(`Ваш любимый жанр под номером ${i}`, "");
-      while (genre === null || genre === "");
-      this.genres[i - 1] = genre;
-    }
-    this.genres.forEach((item, i) => {
-      console.log(`Любимый жанр #${i + 1} - это ${item}`);
-    });
-  },
+//     for (let i = 1; i <= 3; i++) {
+//       do {genre = prompt(`Ваш любимый жанр под номером ${i}`, "");}
+//       while (genre === null || genre === "");
+//       this.genres[i - 1] = genre;
+//     }
+//     this.genres.forEach((item, i) => {
+//       console.log(`Любимый жанр #${i + 1} - это ${item}`);
+//     });
+//   },
 
-  toggleVisibleMyDB: function () {
-    this.privat = !this.privat;
-  },
+//   toggleVisibleMyDB: function () {
+//     this.privat = !this.privat;
+//   },
+// };
+
+// personalMovieDB.start();
+// personalMovieDB.setMyFilms();
+// personalMovieDB.writeYourGenres();
+// personalMovieDB.personalLevel();
+// personalMovieDB.showMyDB();
+
+
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
-personalMovieDB.start();
-personalMovieDB.setMyFilms();
-personalMovieDB.writeYourGenres();
-personalMovieDB.personalLevel();
-personalMovieDB.showMyDB();
+const promosAdv = document.querySelector('.promo__adv'),
+      promos = promosAdv.querySelectorAll('img'),
+      bg = document.querySelector('.promo__bg'),
+      films = document.querySelectorAll('.promo__interactive-item'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+
+promos.forEach(item => {
+    item.style.display = "none";
+});
+document.querySelector('.promo__genre').textContent = "ДРАМА";
+
+
+bg.style.cssText = 'background:url("../img/bg.jpg") center center/cover no-repeat';
+
+movieDB.movies.sort();
+
+movieList.innerHTML = "";
+
+movieDB.movies.forEach((item, i) =>{
+    movieList.innerHTML += `<li class="promo__interactive-item">${i+1}. ${item}
+    <div class="delete"></div></li>`;
+});
